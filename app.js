bf0b26f2c5f932132392218b07b633a45ea4bb12
stariel <ariel.altaras@gmail.com>
2017-08-14 14:33:36 -0700

    set up variables and standard html text

---
+++ app.js
@@ -0,0 +1,34 @@
+var pikeStore = {
+  location: '1st and Pike',
+  minCust: 23,
+  maxCust: 65,
+  avgCookie: 6.3,
+};
+
+var seatacStore = {
+  location: 'SeaTac Airport',
+  minCust: 3,
+  maxCust: 24,
+  avgCookie: 1.2,
+};
+
+var seacenStore = {
+  location: 'Seattle Center',
+  minCust: 11,
+  maxCust: 38,
+  avgCookie: 3.7,
+};
+
+var capHillStore = {
+  location: 'Capitol Hill',
+  minCust: 20,
+  maxCust: 38,
+  avgCookie: 2.3,
+};
+
+var alkiStore = {
+  location: 'Alki',
+  minCust: 2,
+  maxCust: 16,
+  avgCookie: 4.6,
+};
