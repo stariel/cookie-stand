890a68ca8d5fac51c09333fb7bd581d130564878
stariel <ariel.altaras@gmail.com>
2017-08-14 17:02:15 -0700

    created for loop for function and stored answers in an array

--- app.js
+++ app.js
@@ -5,8 +5,19 @@ var pikeStore = {
   minCust: 23,
   maxCust: 65,
   avgCookie: 6.3,
-  salesPerHour: function() {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+  dailySales: [],
+  custPerHour: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);},
+  salesModel: function(){
+    var totalSales = 0;
+    for (var i = 6; i <= 20; i++) {
+      hourlySales = Math.round((this.custPerHour() * this.avgCookie));
+      totalSales = (totalSales + hourlySales);
+      this.dailySales.push(hourlySales);
+      console.log (hourlySales);
+    }
+    console.log (totalSales);
+    this.dailySales.push(totalSales);
   }
 };
 
@@ -15,7 +26,7 @@ var seatacStore = {
   minCust: 3,
   maxCust: 24,
   avgCookie: 1.2,
-  salesPerHour: function() {
+  custPerHour: function() {
     return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
   }
 };
@@ -25,7 +36,7 @@ var seacenStore = {
   minCust: 11,
   maxCust: 38,
   avgCookie: 3.7,
-  salesPerHour: function() {
+  custPerHour: function() {
     return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
   }
 };
@@ -35,7 +46,7 @@ var capHillStore = {
   minCust: 20,
   maxCust: 38,
   avgCookie: 2.3,
-  salesPerHour: function() {
+  custPerHour: function() {
     return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
   }
 };
@@ -45,7 +56,7 @@ var alkiStore = {
   minCust: 2,
   maxCust: 16,
   avgCookie: 4.6,
-  salesPerHour: function() {
+  custPerHour: function() {
     return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
   }
 };
